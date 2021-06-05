3400baa5a2f8eedd83d615e40d0b54fd0db15160
YoungMin KIM <47775179+winston1214@users.noreply.github.com>
2021-06-05 04:45:41 +0000

    Update bert_test.py

--- KED/bert_test.py
+++ KED/bert_test.py
@@ -14,35 +14,50 @@ from kobert.pytorch_kobert import get_pytorch_kobert_model
 import argparse
 from transformers import AdamW
 from transformers.optimization import get_cosine_schedule_with_warmup
-def bert_test(opt):    
-    class BERTClassifier(nn.Module):
-        def __init__(self,
-                     bert,
-                     hidden_size = 768,
-                     num_classes = opt.classes, # softmax 사용 <- binary일 경우는 2
-                     dr_rate=None,
-                     params=None):
-            super(BERTClassifier, self).__init__()
-            self.bert = bert
-            self.dr_rate = dr_rate
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                    bert,
+                    hidden_size = 768,
+                    num_classes = 77, # softmax 사용 <- binary일 경우는 2
+                    dr_rate=None,
+                    params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
 
-            self.classifier = nn.Linear(hidden_size , num_classes)
-            if dr_rate:
-                self.dropout = nn.Dropout(p=dr_rate)
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
 
-        def gen_attention_mask(self, token_ids, valid_length):
-            attention_mask = torch.zeros_like(token_ids)
-            for i, v in enumerate(valid_length):
-                attention_mask[i][:v] = 1
-            return attention_mask.float()
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
 
-        def forward(self, token_ids, valid_length, segment_ids):
-            attention_mask = self.gen_attention_mask(token_ids, valid_length)
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
 
-            _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
-            if self.dr_rate:
-                out = self.dropout(pooler)
-            return self.classifier(out)
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                    pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+def bert_test(opt): 
+
 
     device = torch.device('cuda:{}'.format(opt.device))
     model = torch.load(opt.weights)
@@ -55,20 +70,7 @@ def bert_test(opt):
         train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
         return train_acc
 
-    class BERTDataset(Dataset):
-        def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
-                     pad, pair):
-            transform = nlp.data.BERTSentenceTransform(
-                bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
 
-            self.sentences = [transform([i[sent_idx]]) for i in dataset]
-            self.labels = [np.int32(i[label_idx]) for i in dataset]
-
-        def __getitem__(self, i):
-            return (self.sentences[i] + (self.labels[i], ))
-
-        def __len__(self):
-            return (len(self.labels))
     tokenizer = get_tokenizer()
     tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
     max_len = 256 # 해당 길이를 초과하는 단어에 대해선 bert가 학습하지 않음
@@ -106,7 +108,7 @@ if __name__ == '__main__':
     parser.add_argument('--weights',type=str,default='bert.pt',help='weight_model path')
     parser.add_argument('--save_csv_name', type=str, default='', help='save csv name')
     parser.add_argument('--device', type=int, default=0, help='cuda 0 or 1 or ..')
-    parser.add_argument('--classes',type=int,default=21,help='class num')
+    # parser.add_argument('--classes',type=int,default=21,help='class num')
     parser.add_argument('--batch',type=int,default=32,help='batch-size')
     opt = parser.parse_args()
     bert_test(opt)
